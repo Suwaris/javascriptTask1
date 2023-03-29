f6f97f449638852d2d38532e35862974cf91e556
Ann <vansuwaris@gmail.com>
2023-03-29 16:51:17 +0530

    cahnge the script file

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ var form = document.getElementById("myForm")
 form.addEventListener('submit',function(e){
     e.preventDefault() //prevent auto submit the form
 
-    var search = document.getElementById("serach").value
+
+    var search = document.getElementById('search').value
 
     var originalName = search.split(' ').join('')
-    alert(originalName)
+    //alert(originalName)
 
-    fetch(`https://api.github.com/users?q=${originalName}`)
+    var token = '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';
+    var headers = new Headers();
+    headers.append('Authorization', 'Bearer ' + token);
+
+    fetch(`https://api.github.com/users/${originalName}?q=Q`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
     .then((result) => result.json())
     .then((data) => {
         if(data.message){
@@ -16,8 +25,22 @@ form.addEventListener('submit',function(e){
         } else{
             console.log(data)
             document.getElementById("result").innerHTML = `
-            <img src="${data.avatar_url}"/>
-            <p>${data.name} (${data.login})</p>
+
+            <a href="https://www.github.com/${originalName}"><img src="${data.avatar_url}" class="avatar"/></a>
+            <p class="gitname">${data.name}</p>
+            <p class="gitUname">${data.login}</p>
+
+            <div class="data">           
+            <i class="fa fa-user-o" aria-hidden="true" id="userIcon"><p>${data.followers } Followers ${data.following } Following ${data.public_repos } Repositories</p></i>
+            <div>
+            <i class="fa fa-user-o" aria-hidden="true"><p>${data.location }</p></i>
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">Bio</h5>
+                      <p>${data.bio}</p>
+                    </div>
+                </div>   
+            </div>
             `
         }
        
