2a7bcecd8e10d059a4ee1b0df4504a348498ae0b
Ann <vansuwaris@gmail.com>
2023-03-30 13:00:18 +0530

    change some functionalities in script file

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ form.addEventListener('submit',function(e){
     var originalName = search.split(' ').join('')
     //alert(originalName)
 
-    var token = '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';
+    var token = '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';
     var headers = new Headers();
     headers.append('Authorization', 'Bearer ' + token);
 
+    //get user details
     fetch(`https://api.github.com/users/${originalName}?q=Q`, {
         headers: {
             'Authorization': `Bearer ${token}`
@@ -26,19 +27,52 @@ form.addEventListener('submit',function(e){
             console.log(data)
             document.getElementById("result").innerHTML = `
 
-            <a href="https://www.github.com/${originalName}"><img src="${data.avatar_url}" class="avatar"/></a>
-            <p class="gitname">${data.name}</p>
-            <p class="gitUname">${data.login}</p>
+            <div class="detailBox">
+                <a href="https://www.github.com/${originalName}"><img src="${data.avatar_url}" class="avatar"/></a>
+                <p class="gitname">${data.name}</p>
+                <p class="gitUname">${data.login}</p>
 
-            <div class="data">           
-            <i class="fa fa-user-o" aria-hidden="true" id="userIcon"><p>${data.followers } Followers ${data.following } Following ${data.public_repos } Repositories</p></i>
-            <div>
-            <i class="fa fa-user-o" aria-hidden="true"><p>${data.location }</p></i>
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                      <h5 class="card-title">Bio</h5>
-                      <p>${data.bio}</p>
-                    </div>
+                <div class="location">
+                    <p>${data.location}</p>
+                </div>
+
+                <div class="bio">
+                    <p>${data.bio}</p>
+                </div>
+
+                <div class="data">                          
+                    <p>${data.followers } Followers ${data.following } Following ${data.public_repos } Repositories</p>
+                </div>        
+            </div>
+            
+            `
+        }
+       
+    }).catch(err => {
+        console.log(err)
+    })
+
+
+    //repository details
+    fetch(`https://api.github.com/users/${originalName}/repos`, {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            Accept: "application/vnd.github+json"
+        }
+    })
+    .then((result1) => result1.json())
+    .then((result1) => console.log("error".data1))
+    .then((data1) => {
+        if(data1.ok){
+            document.getElementById("result1").innerHTML = `<h3>Repositories Not Found</h3>`
+        } else{
+            console.log(data1)
+            document.getElementById("result1").innerHTML = `
+            
+            <div class="card" style="width: 75%;">
+                <div class="card-body">
+                    <h5 class="card-title">Repositories</h5>
+                    <p>${data1.repos}</p>
                 </div>   
             </div>
             `
